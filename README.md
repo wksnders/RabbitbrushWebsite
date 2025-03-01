d8a00b0f577d167980091a08139bb05bfa7527d3
wksnders <42388072+wksnders@users.noreply.github.com>
2025-03-01 12:50:22 -0700

    Initial Commit with CI/CD

---
+++ README.md
@@ -0,0 +1,4 @@
+# CICDExplore
+
+This project is a quick exploration of CI/CD to github pages. 
+When new code is checked, in the github pages site for this project will be updated by github actions.
---
+++ src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="about">
+		<h1>This is an about page</h1>
+		<p>goats are here now! they've taken over the about page and are beginning their conquest!</p>
+		<img src="https://i.imgur.com/FwnVqcr.jpeg" />
+	</div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+	.about {
+		min-height: 100vh;
+		display: flex;
+		align-items: center;
+	}
+}
+</style>
