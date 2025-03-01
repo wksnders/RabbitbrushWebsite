e212630187bbcb467b67b215a8f70aa1ca0545a7
wksnders <42388072+wksnders@users.noreply.github.com>
2025-03-01 14:37:58 -0700

    header content

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,24 @@
 </script>
 
 <template>
+  <div class="hero-section">
+    <h1>Rabbitbrush Studio</h1>
+    <h2>Art Classes <span class="small">for</span> All Ages</h2>
+  </div>
 </template>
 
 <style scoped>
+
+h1{
+  font-size:72px;
+
+}
+
+h2{
+  font-size:64px;
+}
+
+.small{
+  font-size:36px;
+}
 </style>
