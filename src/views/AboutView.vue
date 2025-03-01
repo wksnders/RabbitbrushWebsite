6b67b0d79c3533d8747c339dd4d802855fbd8a49
wksnders <42388072+wksnders@users.noreply.github.com>
2025-03-01 13:54:34 -0700

    about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,12 @@
 <template>
 	<div class="about">
-		<h1>This is an about page</h1>
-		<p>goats are here now! they've taken over the about page and are beginning their conquest!</p>
-		<img src="https://i.imgur.com/FwnVqcr.jpeg" />
+		<h1>Teaching Art is my joy</h1>
+		<p>My name is Annette Emmer.  I am a past president of the Associated Utah Artists, an organization which began over sixty years ago and was the first professional artists’ association in Utah. I was a member for more than 20 years (serving as an officer and secretary for 15 of those years), until it disbanded in 2007. My work has been exhibited at the Kimball Arts Center in Park City, Utah; The Springville Art Museum, Springville, Utah; and various other galleries and exhibitions throughout Utah.</p>
+
+    <p>I am honored to have received several awards throughout the years: in the fine arts professional division at the Utah State Fair, the Salt Lake County Fair, and private exhibitions. My work also hangs in private collections throughout the United States. I work primarily with pencil, pen and ink, watercolor, acrylics, oils, mixed media, and hand-built ceramics. I started Rabbitbrush Studio so that I could share my love of art, and my skills, with people of all ages.  </p>
+		<img src="https://web.archive.org/web/20190319011131im_/http://nebula.wsimg.com/23058e0a2e4a52291a4e4edca736f08c?AccessKeyId=764A40C494058CC0EF34&disposition=0&alloworigin=1" />
 	</div>
+  <div>Carousel placeholder</div>
 </template>
 
 <style>
