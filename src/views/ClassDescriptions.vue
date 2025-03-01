c434f541de075c672f62c7c5a12ce58e66916e4d
wksnders <42388072+wksnders@users.noreply.github.com>
2025-03-01 14:01:13 -0700

    descriptions

--- src/views/ClassDescriptions.vue
+++ src/views/ClassDescriptions.vue
@@ -1,9 +1,31 @@
 <template>
-	<div class="about">
-		<h1>This is an about page</h1>
-		<p>goats are here now! they've taken over the about page and are beginning their conquest!</p>
-		<img src="https://i.imgur.com/FwnVqcr.jpeg" />
-	</div>
+	<article class="descriptions">
+		<h1>Class Descriptions</h1>
+    <section>
+      <p>Through art education children begin to notice and appreciate colors and subtle details in the world around them.  With encouragement and guidance they learn to express themselves visually, building self confidence and self esteem and developing the courage to be a creator and an explorer of their world.</p>
+      <p>In my classes, I focus on the skills necessary for students to become familiar with several different media, and learn enough control over them to be able to create their own works of art. I use a positive approach to direct and encourage my students.</p>
+    </section>
+    <section>
+      <h3>Children's After School Classes</h3>
+      <p>Classes rotate from drawing in black and white (graphite, charcoal, pen and ink) to adding color (colored pencil, soft pastels, oil pastels), to painting in watercolors and acrylic. We enhance these art and design skills through ceramic clay projects. All supplies included.</p>
+      <p><strong>Children's after school classes are $60/month per student. Classes are 1 1/2 hours long and are held 4 times a month. If there is a 5th week in a month, classes will not be held that week. All supplies included.</strong></p>
+    </section>
+    <section>
+      <h3>Children's After School Classes</h3>
+      <p>Classes rotate from drawing in black and white (graphite, charcoal, pen and ink) to adding color (colored pencil, soft pastels, oil pastels), to painting in watercolors and acrylic. We enhance these art and design skills through ceramic clay projects. All supplies included.</p>
+    </section>
+    <section>
+      <h3>Adult Art Classes (Ages 12 and up)</h3>
+      <p> First and Third Wednesday of the month 7:30-9:30 PM
+          (No classes on the second, fourth, or fifth Wednesdays)
+      </p>
+      <p>First Wednesday of each month, $20.00. 7:30pm to 9:30 pm, a full art class with a new project each month, all supplies included. We will use a variety of media including pencil, colored pencils, oil pastel, charcoal, watercolor and acrylic. Maybe hand built ceramic sculpture! Adult students must register for these classes.</p>
+      <p>Third Wednesday of each month, add-on Studio Night! 7:30-9:30 pm is an Open Studio night where regular adult students can come to the studio and finish anything they didn't get done on the previous class, or get a new paper/canvas and practice the same technique again, or do their own picture with studio supplies. I will be available for assistance or questions, but no new project will be taught. This is not a stand-alone class, but is only open to regular adult students. Adult students must preregister for the Studio Night. $32/month for both the First Wednesday class and the Third Wednesday Studio Night. </p>
+      <p>You do not need to take the Open Studio night on the third Wednesday if you just want to take the first Wednesday class.</p>
+    </section>
+
+		<!--<img src="https://i.imgur.com/FwnVqcr.jpeg" />-->
+	</article>
 </template>
 
 <style>
