ea942ffe4435163ddcb0da309d41c1da6cada14c
wksnders <42388072+wksnders@users.noreply.github.com>
2025-03-01 14:08:21 -0700

    testimonials section copy

--- src/views/Testimonials.vue
+++ src/views/Testimonials.vue
@@ -1,8 +1,17 @@
 <template>
 	<article class="about">
-		<h1>This is an about page</h1>
-		<p>goats are here now! they've taken over the about page and are beginning their conquest!</p>
-		<img src="https://i.imgur.com/FwnVqcr.jpeg" />
+    <section>
+      <h1>Why Take my classes?</h1>
+      <p>"These classes are incredible! My kids are having so much fun, and our house is filled with the beautiful art that they're creating. They're so proud to show off their artwork to everyone who comes to visit." -- Trista E.</p>
+      <p>"My daughter is gaining a lot of confidence, while exploring a variety of mediums and techniques. I highly recommend these classes." -- Nicole Nuzman Laird</p>
+      <p>"My little girl can't wait to take another class." -- Petrea Harris Loertscher</p>
+      <p>"My daughter loves taking art classes from Annette." -- Caitlin Stevenson</p>
+      <p>"I was at a loss trying to find a constructive outlet for my son’s interest in art.  I searched for a long time to find the perfect fit for him, and I definitely found it at Rabbitbrush Studio.  Annette is perfect for my son.  She’s encouraging and attentive, and stretches him without pressuring him.  With her, art is fun and challenging.  She is a positive teacher and I’ve loved seeing my son’s talent develop under her skilled care." -- Charity Flanagan</p>
+      <p>"My daughter started art classes with Annette in the summer and has really enjoyed all the different classes that she attended. My daughter is very particular about her work and Annette is always so patient with her, never makes her feel rushed and is very encouraging!" -- Nicole Costello</p>
+      <p>'Mrs. Emmer's extensive knowledge of art in different mediums has taught my children to expand their art abilities. My children have loved taking art lessons from Annette.  She provides a relaxed, fun and encouraging environment where people of all ages can express their creativity.' -- Sheree Hartley</p>
+      <p>"Annette knows what she's doing, taking this art class was one of the best decisions I’ve made. I learned so many different techniques and styles of art. It really gave me a head start for future classes." -- Former Child Student</p>
+      <p>"I am so impressed by her classes, there's a wide range of work that we do, and Annette always helps you out so you're not left there not knowing what to do. Best class ever!"-- Former Child Student</p>
+    </section>
   </article>
 </template>
 
