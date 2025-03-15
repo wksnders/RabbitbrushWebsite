559ef8f353d2953699fac7bc07a04a3fdd5a82ab
wksnders <42388072+wksnders@users.noreply.github.com>
2025-03-14 19:22:31 -0600

    updated header and about page copy

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -14,12 +14,15 @@ h1{
   font-size:72px;
 	color: var(--color-heading);
   font-family: 'Sacramento';
+  font-weight: 400;
 }
 
 h2{
+  padding-left: 10%;
   font-size:64px;
 	color: var(--color-heading);
-  font-family: 'sanchez';
+  font-family: 'Sacramento';
+  font-weight: 400;
 }
 
 .small{
